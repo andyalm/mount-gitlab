82803c594304ffcc12967f8dcf6466dbec28dcba
Andy Alm <andy.alm@gettyimages.com>
2021-12-01 17:19:44 -0800

    Start generating module manifest dynamically so we can version off of the tag name

---
+++ MountGitlab/CreateManifest.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference='Stop'
+
+$ModuleVersion='0.0.1'
+if($env:GITHUB_REF_NAME -and $env:GITHUB_REF_NAME -match '^v(?<Version>\d+\.\d+\.\d+)$') {
+    $ModuleVersion = $Matches.Version
+}
+
+New-ModuleManifest -Path ../bin/MountGitlab/MountGitlab.psd1 `
+    -RootModule 'MountGitlab.dll' `
+    -ModuleVersion $ModuleVersion `
+    -Guid '5e4bb943-62b6-4cde-9bf7-45fea047ce11' `
+    -Author 'Andy Alm' `
+    -Copyright '(c) 2021 Andy Alm. All rights reserved.' `
+    -Description 'An experimental powershell provider that allows you to explore gitlab as a filesystem.' `
+    -PowerShellVersion '7.2' `
+    -FormatsToProcess @('Formats.ps1xml') `
+    -FunctionsToExport @() `
+    -VariablesToExport @() `
+    -CmdletsToExport @() `
+    -AliasesToExport @()
