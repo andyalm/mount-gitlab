1eb3432e4540fd4e11dc84f8f2d6b1275add93df
Andy Alm <andy.alm@gettyimages.com>
2021-12-02 14:24:51 -0800

    Fix the publish script to work after adding GitlabCli as an official dependency

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,6 @@
 $ErrorActionPreference='Stop'
 
+if(-not (Get-Module GitlabCli)) {
+    Install-Module -Name GitlabCli -Force
+}
 Publish-Module -Path ./bin/MountGitlab -NuGetApiKey $env:NuGetApiKey
