1a836b45b4c923cb167d9829451e3bdc84f77cd1
Andy Alm <andy.alm@gettyimages.com>
2021-12-01 16:36:16 -0800

    Update publish script to halt on error

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,4 @@
-cd MountGitlab/bin/Release/net6.0
-Import-Module MountGitlab.psd1
+$ErrorActionPreference='Stop'
+
+Import-Module ./MountGitlab/bin/Release/net6.0/MountGitlab.psd1
 Publish-Module -Name MountGitlab -NuGetApiKey $env:NuGetApiKey
